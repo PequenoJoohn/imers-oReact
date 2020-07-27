afae4abf25b1b922ee89fd7d28fbd121b04e400d
pequenojoohn <kheymk@hotmail.com>
2020-07-27 14:09:54 -0300

    Add new videos.

--- src/App.js
+++ src/App.js
@@ -24,21 +24,27 @@ function App() {
       />
 
       <Carousel
+        ignoreFirstVideo
         category={dadosIniciais.categorias[1]} />
 
       <Carousel
+        ignoreFirstVideo
         category={dadosIniciais.categorias[2]} />
 
       <Carousel
+        ignoreFirstVideo
         category={dadosIniciais.categorias[3]} />
 
       <Carousel
+        ignoreFirstVideo
         category={dadosIniciais.categorias[4]} />
 
       <Carousel
+        ignoreFirstVideo
         category={dadosIniciais.categorias[5]} />
 
       <Carousel
+        ignoreFirstVideo
         category={dadosIniciais.categorias[6]} />
 
       <Footer />
