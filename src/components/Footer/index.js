8e816f61f8da641568784e9b320aad71dc6d5644
pequenojoohn <kheymk@hotmail.com>
2020-07-27 13:26:09 -0300

    Add Component Musics

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -14,6 +14,7 @@ function Footer() {
           Imersão React da Alura
         </a>
       </p>
+      <p>Desenvolvido por <a href="https://www.linkedin.com/in/jonathanlm/">Jonathan Matos</a></p>
     </FooterBase>
   );
 }
